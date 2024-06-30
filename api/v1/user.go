86635b90d7faa734326eebf633471c60d4e248e2
alvindo <alvindo56@gmail.com>
2024-06-30 20:45:32 +0700

    feat: user endpoint :rocket:

---
+++ api/v1/user.go
@@ -0,0 +1,45 @@
+package handler
+
+import (
+	"fmt"
+	. "github.com/tbxark/g4vercel"
+	"net/http"
+)
+
+func User(w http.ResponseWriter, r *http.Request) {
+	server := New()
+
+	server.Use(Recovery(func(err interface{}, c *Context) {
+		if httpError, ok := err.(HttpError); ok {
+			c.JSON(httpError.Status, H{
+				"message": httpError.Error(),
+			})
+		} else {
+			message := fmt.Sprintf("%s", err)
+			c.JSON(500, H{
+				"message": message,
+			})
+		}
+	}))
+
+	server.GET("/user", func(context *Context) {
+		name := context.Query("name")
+		if name == "" {
+			context.JSON(400, H{
+				"message": "name not found",
+			})
+		} else {
+			context.JSON(200, H{
+				"data": fmt.Sprintf("Hello %s!", name),
+			})
+		}
+	})
+
+	server.GET("/user/:id", func(context *Context) {
+		context.JSON(400, H{
+			"data": H{
+				"id": context.Param("id"),
+			},
+		})
+	})
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-online-shop-api
 
 go 1.22.2
+
+require github.com/tbxark/g4vercel v0.0.4
